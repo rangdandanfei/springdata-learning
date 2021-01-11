5c208e990a0009659708ac2dfcf52d0220113ab8
rangdandanfei <875123970@qq.com>
2021-01-11 10:50:17 +0800

    项目本地仓库上传

---
+++ src/test/java/com/fan/springdatalearning/SpringdataLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.springdatalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
